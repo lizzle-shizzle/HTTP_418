cb94266d04ca7b0331cc9546269812653f05cdab
Thorne199 <michael@itecology.co.za>
2016-07-11 18:46:53 +0200

    Commit ViewCultivationFrequency HTML CSS JS

--- Code/JS/script.js
+++ Code/JS/script.js
@@ -1,4 +1,9 @@
 var app = angular.module('Harvest', []);
-	app.controller('dummy', function($scope) {
-    $scope.dummy = ["Dummy1", "Dummy2", "Dummy3", "Dummy4"];
+	app.controller('selectCultivationFrequency', function($scope) {
+$scope.cultivationFrequency = [
+	{"Name" : "Monthly", "Description" : "Collect crop on a monthly basis."},
+	{"Name" : "Weekly", "Description" : "Collect crop on a weekly basis."},
+	{"Name" : "Daily", "Description" : "Collect crop on a daily basis."},
+	{"Name" : "Annually", "Description" : "Collect crop on an annual basis."}
+];
 });
