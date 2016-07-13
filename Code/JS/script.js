3951752abfe1d7b1976b261e73f71d28c45ee2f7
Thorne199 <michael@itecology.co.za>
2016-07-13 14:40:39 +0200

    Commit EditOrchardBlock HTML JS

--- Code/JS/script.js
+++ Code/JS/script.js
@@ -1,4 +1,18 @@
 var app = angular.module('Harvest', []);
-	app.controller('dummy', function($scope) {
-    $scope.dummy = ["Dummy1", "Dummy2", "Dummy3", "Dummy4"];
+	app.controller('selectIrrigation', function($scope) {
+    $scope.irrigation = ["Surface", "Sprinkler", "Drip/trickle", "Subsurface"];
 });
+	app.controller('selectCrop', function($scope) {
+    $scope.crop = ["Mangoes", "Litchis", "Macadamias", "Avos"];
+});   
+app.controller('selectCultivationFrequency', function($scope) {
+$scope.cultivationFrequency = [
+	{"Name" : "Monthly", "Description" : "Collect crop on a monthly basis."},
+	{"Name" : "Weekly", "Description" : "Collect crop on a weekly basis."},
+	{"Name" : "Daily", "Description" : "Collect crop on a daily basis."},
+	{"Name" : "Annually", "Description" : "Collect crop on an annual basis."}
+];
+});
+	app.controller('selectYieldMeasurement', function($scope) {
+    $scope.yieldMeasurement = ["Bag", "kg", "Truckload", "Unit"];
+}););
