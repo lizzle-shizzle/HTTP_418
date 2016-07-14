f0320338c4188a9f8f7a242c6b60a0e69e8f3c21
Einsteinium1458 <meyers.charl@gmail.com>
2016-07-14 16:33:18 +0200

    Made single page application for orchard block

--- Code/JS/script.js
+++ Code/JS/script.js
@@ -1,26 +1,31 @@
 var app = angular.module('Harvest', ['ngRoute']);
-app.controller('selectCropType', function($scope) {
-	$scope.cropType = [
-	{"Type" : "Avocado", "Soil" : "Base"},
-	{"Type" : "Mango", "Soil" : "Sour"},
-	{"Type" : "Macadamia", "Soil" : "Dry"}
+app.controller('selectOrchardBlock', function($scope) {
+	$scope.orchardBlock = [
+	{"Name":"Field 1", "CropYield":20, "Hectares":21.5, "RevenueGenerated":20124, "BlockDimentions":"12m X 16m", "CropType":"Mango", "IrrigationType":"Sprinkler", "DatePlanted":2016-02-03, "CultivationFrequency":"Monthly", "YieldMeasurementType":"Basket"/*, "GPSCoordinates":"-26.158518, 28.257179"*/},
+	{"Name":"Field 2", "CropYield":23, "Hectares":12, "RevenueGenerated":1512, "BlockDimentions":"4m X 3m", "CropType":"Mango", "IrrigationType":"Sprinkler", "DatePlanted":2016-02-04, "CultivationFrequency":"Monthly", "YieldMeasurementType":"Basket"/*, "GPSCoordinates":"-26.158518, 28.257179"*/},
+	{"Name":"Field 3", "CropYield":30, "Hectares":23.6, "RevenueGenerated":6262, "BlockDimentions":"12.2m X 16m", "CropType":"Mango", "IrrigationType":"Sprinkler", "DatePlanted":2016-02-05, "CultivationFrequency":"Monthly", "YieldMeasurementType":"Basket"/*, "GPSCoordinates":"-26.158518, 28.257179"*/},
+	{"Name":"Field 4", "CropYield":78, "Hectares":50, "RevenueGenerated":1515115, "BlockDimentions":"20m X 16m", "CropType":"Mango", "IrrigationType":"Sprinkler", "DatePlanted":2016-02-06, "CultivationFrequency":"Monthly", "YieldMeasurementType":"Basket"/*, "GPSCoordinates":"-26.158518, 28.257179"*/}
 ];
 });
 
 app.config(function($routeProvider/*, $locationProvider*/) {	
 	$routeProvider
-		.when('/CropType', {
-			templateUrl: "./viewCropType.html",
-			controller: "selectCropType"
+		.when('/OrchardBlock', {
+			templateUrl: "./viewOrchardBlock.html",
+			controller: "selectOrchardBlock"
 		})
 		.when('/Create', {
-			templateUrl: "./createCroptype.html",
-			controller: "Log"
+			templateUrl: "./createOrchardBlock.html",
+			controller: "OrchardBlock"
 		})
 		.when("/Edit", {
-			templateUrl: "./editCropType.html",
-			controller: "selectCropType"
+			templateUrl: "./editOrchardBlock.html",
+			controller: "selectOrchardBlock"
 		})
-		/*.otherwise({redirectTo: "/"})*/;
+		.when("/", {
+			templateUrl: ".",
+			controller: "selectOrchardBlock"
+		})
+		.otherwise({redirectTo: "/"});
 		//$locationProvider.html5Mode({enabled: true, requireBase: false});
 });
