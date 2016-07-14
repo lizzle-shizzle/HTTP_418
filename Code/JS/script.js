a33001c7a622f42b8754ca74acfd3ca98b27efed
Einsteinium1458 <meyers.charl@gmail.com>
2016-07-14 17:26:41 +0200

    Commit createOrchardBlock.html, editOrchardBlock.html, script.js, viewOrchardBlock.html

--- Code/JS/script.js
+++ Code/JS/script.js
@@ -1,10 +1,10 @@
 var app = angular.module('Harvest', ['ngRoute']);
 app.controller('selectOrchardBlock', function($scope) {
 	$scope.orchardBlock = [
-	{"Name":"Field 1", "CropYield":20, "Hectares":21.5, "RevenueGenerated":20124, "BlockDimentions":"12m X 16m", "CropType":"Mango", "IrrigationType":"Sprinkler", "DatePlanted":2016-02-03, "CultivationFrequency":"Monthly", "YieldMeasurementType":"Basket"/*, "GPSCoordinates":"-26.158518, 28.257179"*/},
-	{"Name":"Field 2", "CropYield":23, "Hectares":12, "RevenueGenerated":1512, "BlockDimentions":"4m X 3m", "CropType":"Mango", "IrrigationType":"Sprinkler", "DatePlanted":2016-02-04, "CultivationFrequency":"Monthly", "YieldMeasurementType":"Basket"/*, "GPSCoordinates":"-26.158518, 28.257179"*/},
-	{"Name":"Field 3", "CropYield":30, "Hectares":23.6, "RevenueGenerated":6262, "BlockDimentions":"12.2m X 16m", "CropType":"Mango", "IrrigationType":"Sprinkler", "DatePlanted":2016-02-05, "CultivationFrequency":"Monthly", "YieldMeasurementType":"Basket"/*, "GPSCoordinates":"-26.158518, 28.257179"*/},
-	{"Name":"Field 4", "CropYield":78, "Hectares":50, "RevenueGenerated":1515115, "BlockDimentions":"20m X 16m", "CropType":"Mango", "IrrigationType":"Sprinkler", "DatePlanted":2016-02-06, "CultivationFrequency":"Monthly", "YieldMeasurementType":"Basket"/*, "GPSCoordinates":"-26.158518, 28.257179"*/}
+	{"Name":"Field 1", "CropYield":20, "Hectares":21.5, "RevenueGenerated":20124, "BlockDimentions":"12m X 16m", "CropType":"Mango", "IrrigationType":"Sprinkler", "DatePlanted":"2016-02-03", "CultivationFrequency":"Monthly", "YieldMeasurementType":"Basket"/*, "GPSCoordinates":"-26.158518, 28.257179"*/},
+	{"Name":"Field 2", "CropYield":23, "Hectares":12, "RevenueGenerated":1512, "BlockDimentions":"4m X 3m", "CropType":"Mango", "IrrigationType":"Sprinkler", "DatePlanted":"2016-02-04", "CultivationFrequency":"Monthly", "YieldMeasurementType":"Basket"/*, "GPSCoordinates":"-26.158518, 28.257179"*/},
+	{"Name":"Field 3", "CropYield":30, "Hectares":23.6, "RevenueGenerated":6262, "BlockDimentions":"12.2m X 16m", "CropType":"Mango", "IrrigationType":"Sprinkler", "DatePlanted":"2016-02-05", "CultivationFrequency":"Monthly", "YieldMeasurementType":"Basket"/*, "GPSCoordinates":"-26.158518, 28.257179"*/},
+	{"Name":"Field 4", "CropYield":78, "Hectares":50, "RevenueGenerated":1515115, "BlockDimentions":"20m X 16m", "CropType":"Mango", "IrrigationType":"Sprinkler", "DatePlanted":"2016-02-06", "CultivationFrequency":"Monthly", "YieldMeasurementType":"Basket"/*, "GPSCoordinates":"-26.158518, 28.257179"*/}
 ];
 });
 
@@ -16,7 +16,7 @@ app.config(function($routeProvider/*, $locationProvider*/) {
 		})
 		.when('/Create', {
 			templateUrl: "./createOrchardBlock.html",
-			controller: "OrchardBlock"
+			controller: "selectOrchardBlock"
 		})
 		.when("/Edit", {
 			templateUrl: "./editOrchardBlock.html",
