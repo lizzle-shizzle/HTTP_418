326a092e13038b7c67b5bd35a9b86706e2fd4515
Thorne199 <michael@itecology.co.za>
2016-07-16 12:10:33 +0200

    Commit ViewIrrigationType HTML JS

--- Code/JS/script.js
+++ Code/JS/script.js
@@ -1,4 +1,9 @@
 var app = angular.module('Harvest', []);
-	app.controller('dummy', function($scope) {
-    $scope.dummy = ["Dummy1", "Dummy2", "Dummy3", "Dummy4"];
+app.controller('selectIrrigation', function($scope) {
+$scope.irrigation = [
+	{"Name" : "Surface", "Description" : "Water the surface of the orchard."},
+	{"Name" : "Sprinkler", "Description" : "Water the orchard with a sprinkler."},
+	{"Name" : "Drip/trickle", "Description" : "Trickle water the orchard."},
+	{"Name" : "Subsurface", "Description" : "Water the subsurface of the orchard."}
+];
 });
