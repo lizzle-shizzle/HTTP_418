322fce540d427ff9eff1d5ef91f2a34f2d70067c
Einsteinium1458 <meyers.charl@gmail.com>
2016-07-31 13:36:58 +0200

    Add create Farm action

--- Code/Harvest/api/controllers/FarmController.js
+++ Code/Harvest/api/controllers/FarmController.js
@@ -6,8 +6,30 @@
  */
 
 module.exports = {
-	'new': function(req, res) {
+	//Load the create farm page -> new.ejs
+	new: function(req, res) {
+		//return to homepage if not logged in
+		//cannot create farm if not logged in
+		if (!req.session.me) {
+	      return res.view('homepage');
+	    }
 		res.view({layout: "signedInLayout", title: "Create new farm"});
-	}
+	},
+
+	create: function (req, res) {
+    // Create farm from pramaters sent from
+    // create farm form -> new.ejs
+    Farm.create({
+    	name: req.param("fname"),
+    	size: req.param("fsize")
+    }, function farmCreated(err, farm) {
+    	//If there is an error 
+    	//return appropiate error message
+    	if(err) return res.negotiate(err);
+
+    	//If sucessfull go back to dashboard
+    	return res.redirect("/");
+    });
+  }
 };
 
