475c045dac9b45d3fcc99c7e966aa3f42a870db7
Einsteinium1458 <meyers.charl@gmail.com>
2016-07-31 18:55:31 +0200

    Commit edit farm

--- Code/Harvest/api/controllers/FarmController.js
+++ Code/Harvest/api/controllers/FarmController.js
@@ -17,28 +17,61 @@ module.exports = {
 	},
 
 	create: function (req, res) {
-    // Create farm from pramaters sent from
-    // create farm form -> new.ejs
-    Farm.create({
-    	name: req.param("fname"),
-    	size: req.param("fsize"),
-    	farmer: req.session.me
-    }, function farmCreated(err, farm) {
-    	//If there is an error 
-    	//return appropiate error message
-    	if(err) return res.negotiate(err);
+	    // Create farm from pramaters sent from
+	    // create farm form -> new.ejs
+	    Farm.create({
+	    	name: req.param("fname"),
+	    	size: req.param("fsize"),
+	    	farmer: req.session.me
+	    }, function farmCreated(err, farm) {
+	    	//If there is an error 
+	    	//return appropiate error message
+	    	if(err) return res.negotiate(err);
 
-    	//If farm created sucessfully
-    	//Add to farmer
-    	User.find()
-    	.populate('farms')
-    	.exec(function farmLinked(err, user) {
-    		if(err) return res.negotiate(err);
+	    	//If farm created sucessfully
+	    	//Add to farmer
+	    	User.find()
+	    	.populate('farms')
+	    	.exec(function farmLinked(err, user) {
+	    		if(err) return res.negotiate(err);
 
-    		//If sucessfull go back to dashboard
-    		return res.redirect("/");
-    	});    	
-    });
-  }
+	    		//If sucessfull go back to dashboard
+	    		return res.redirect("/");
+	    	});    	
+	    });
+	},
+
+	//show edit farm -> /farm/edit.ejs
+	edit: function(req, res) {
+		if (!req.session.me) {
+	      return res.view('homepage');
+	    }
+
+	    //Get farm linked to logged in farmer
+	    //send farm as json
+		User.findOne({id: req.session.me})
+		.populate("farms")
+		.exec(function (err, user) {
+			//If there is an error 
+	    	//return appropiate error message
+			if(err) return res.negotiate(err);
+
+			res.view({layout: "signedInLayout", title: "Edit farm", farm: user.farms[0]});
+		});
+	},
+
+	update: function(req, res) {
+		Farm.update({id: req.param("id")}, {
+			name: req.param("fname"),
+			size: req.param("fsize")
+		}, function farmUpdated(err) {
+			//If there is an error 
+	    	//return appropiate error message
+			if(err) return res.negotiate(err);
+
+			//If sucessfull go back to dashboard
+	    	return res.redirect("/");
+		});
+	}
 };
 
