e94507e67942ac45e2a866753c94e7f98bc99cb1
Christiaan Saaiman <u12059138@tuks.co.za>
2016-08-21 00:13:45 +0200

    Initial commit and api creation of Foreman and ForemanShift

---
+++ Code/Harvest/api/controllers/ForemanController.js
@@ -0,0 +1,11 @@
+/**
+ * ForemanController
+ *
+ * @description :: Server-side logic for managing Foremen
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ Code/Harvest/api/models/Foreman.js
@@ -0,0 +1,47 @@
+/**
+ * Foreman.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	//The foreman's full name
+  	//e.g. Adolf Hitler
+
+  	fname: {
+  		type: 'string',
+  		required: true
+  	},
+
+  	lname: {
+  		type: 'string',
+  		required: true
+  	},
+
+  	//The foreman's email address
+  	//e.g. adolf@nazi.com
+  	email: {
+  		type: 'email'//'string',
+  		email: true,
+  		required: true,
+  		unique: true
+  	},
+
+  	//The foreman's password encrypted
+  	//e.g. asdgh8a249321e9dhgaslcbqn2913051#T(@GHASDGA
+  	encryptedPassword: {
+  		type: 'string',
+  		required: true
+  	}
+
+  	//The foreman's multiple shifts
+  	shifts: {
+  		collection: 'foremanshift',
+  		via: 'shift'
+  	}
+  }
+};
+
