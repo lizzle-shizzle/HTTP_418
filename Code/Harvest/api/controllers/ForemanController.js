eedad4a642b02f7441354ad29eba79ee08fef641
u14310156 <u14310156@tuks.co.za>
2016-08-28 17:40:34 +0200

    Add progress so far with maintain & view orchard block allocations
    Stuck with the incorporation of dummy data in order to test
    functionality

---
+++ Code/Harvest/api/controllers/ForemanController.js
@@ -0,0 +1,57 @@
+/**
+ * ForemanController
+ *
+ * @description :: Server-side logic for managing foremen
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	editAllocation: function(req, res, next) {
+	    User.findOne(req.param('id'), function foundUser(err, user) {
+	      if (err) return next(err);
+	      if (!user) return next();
+	      res.view({
+	        user: user
+	      });
+	    });
+	},
+	updateAllocation: function(req, res, next) {
+	    User.update(req.param('id'), req.params.all(), function userUpdated (err) {
+	      if (err) return res.redirect('/foreman/maintainForemanOrchardAllocation' + req.param('id'));
+	     // alert("Success");
+	      /*toastr['success']('Invalid email/password combination.', 'Success', {
+	          closeButton: true
+	        });*/
+	      //res.redirect('/user/show' + req.param('id'));
+	      //return res.redirect('/user/editFarmer' + req.param('id'));
+	      /*if (req.param('email').rule !== 'unique') {
+	        return res.emailAddressInUse();
+	      }*/
+	      return res.view('dashboard', {
+	        me: {
+	          id: req.param('id'),
+	          fname: req.param('fname'),
+	          lname: req.param('lname'),
+	          birthdate: req.param('birthdate'),
+	          email: req.param('email'),
+	          isAdmin: !!req.param('admin'),
+	          gravatarUrl: req.param('gravatarUrl')
+	        }
+	      });
+	    });
+	},
+  	viewAllocation: function(req, res, next) {
+	    User.findOne(req.param('id'), function foundUser(err, user) {
+	      if (err) return next(err);
+	      if (!user) return next();
+	      res.view({
+	        user: user
+	      });
+	    });
+	},
+	checkOrch: function($scope) {
+		$scope.orchards = ["A", "B", "C", "D"];
+	}
+
+};
+
