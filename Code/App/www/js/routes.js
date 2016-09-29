27e13b7ccb11a7bddcaa51747c110760a987135e
Einsteinium1458 <meyers.charl@gmail.com>
2016-09-29 10:18:01 +0200

    Add app interface

---
+++ Code/App/www/js/routes.js
@@ -0,0 +1,61 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.workerYieldPerformance', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/workerYieldPerformance.html',
+        controller: 'workerYieldPerformanceCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.cropYield', {
+    url: '/page3',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/cropYield.html',
+        controller: 'cropYieldCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('splashScreen', {
+    url: '/page6',
+    templateUrl: 'templates/splashScreen.html',
+    controller: 'splashScreenCtrl'
+  })
+
+  .state('login', {
+    url: '/page7',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('about', {
+    url: '/page8',
+    templateUrl: 'templates/about.html',
+    controller: 'aboutCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page6')
+
+  
+
+});
