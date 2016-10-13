b4c0e23c37078ea9f889ec1207ab14aa54bb1511
Christiaan Saaiman <Christiaan Saaiman>
2016-10-13 17:20:01 +0200

    Converted all indentation to tabs

--- Code/Harvest/api/controllers/CropTypeController.js
+++ Code/Harvest/api/controllers/CropTypeController.js
@@ -6,20 +6,20 @@
  */
 
 module.exports = {
-    view: function(req, res) {
-        if (!req.session.me) {
-	      return res.view('homepage');
-	    }
+	view: function(req, res) {
+		if (!req.session.me) {
+		  return res.view('homepage');
+		}
 
 		User.findOne({id: req.session.me})
 		.populate("farms")
 		.exec(function (err, user) {
 			//If there is an error 
-	    	//return appropiate error message
+			//return appropiate error message
 			if(err) return res.negotiate(err);
 
 			if(user.farms[0] == null)
-        		return res.view('farm/new', {layout: "signedInLayout", title: "Create new farm"});
+				return res.view('farm/new', {layout: "signedInLayout", title: "Create new farm"});
 
 			//get farm linked to user and fetch all orchardblocks
 			/*OrchardBlock.find({farm: user.farms[0].id})
@@ -38,53 +38,53 @@ module.exports = {
 				layout: "signedInLayout"});				
 			});
 		});        
-    },
+	},
 
-    new: function(req, res) {
+	new: function(req, res) {
 		//return to homepage if not logged in
 		//cannot create crop type if not logged in
 		if (!req.session.me) {
-	      return res.view('homepage');
-	    }
+		  return res.view('homepage');
+		}
 		User.findOne({id: req.session.me})
 		.populate("farms")
 		.exec(function (err, user) {
 			//If there is an error 
-	    	//return appropiate error message
+			//return appropiate error message
 			if(err) return res.negotiate(err);
 
 			res.view({layout: "signedInLayout", title: "Create crop type"});
 		});				
 	},
 
-    create: function (req, res) {
+	create: function (req, res) {
 		//redirect to homepage if user not logged in
 		if (!req.session.me) {
-	      return res.view('homepage');
-	    }
-	    // Create crop type from pramaters sent from -> new.ejs	    
+		  return res.view('homepage');
+		}
+		// Create crop type from pramaters sent from -> new.ejs	    
 		//Create new crop type, get crop type id and add to orchardblock
-        CropType.create({name: req.param("newCropType")}, function(err, crop) {
-            //If there is an error 
-	    	//return appropiate error message
-	    	if(err) return res.negotiate(err);
+		CropType.create({name: req.param("newCropType")}, function(err, crop) {
+			//If there is an error 
+			//return appropiate error message
+			if(err) return res.negotiate(err);
 			
 			//if successfull send 200 response
 			return res.redirect("/cropType/view");			    	
-        });
+		});
 	},
 
 	edit: function(req, res) {		
 		//return to homepage if not logged in
 		//cannot edit crop type if not logged in
 		if (!req.session.me) {
-	      return res.view('homepage');
-	    }
+		  return res.view('homepage');
+		}
 		User.findOne({id: req.session.me}) 
 		.populate("farms")
 		.exec(function (err, user) {
 			//If there is an error 
-	    	//return appropiate error message
+			//return appropiate error message
 			if(err) return res.negotiate(err);
 
 			//find all crop types
@@ -103,8 +103,8 @@ module.exports = {
 		//return to homepage if not logged in
 		//cannot edit crop type if not logged in
 		if (!req.session.me) {
-	      return res.view('homepage');
-	    }
+		  return res.view('homepage');
+		}
 		//edit OrchardBlock selected in -> edit.ejs
 		/*OrchardBlock.update({id: req.param("orchardID")}, {
 			cropType: req.param('cropTypeID')
