4878f5c8ba20a5f02919373b0a0895d0eda1a7f0
Einsteinium1458 <meyers.charl@gmail.com>
2016-10-18 21:51:11 +0200

    Added more functionality to app
    Changing crop type now shows different foreman and listed foreman are
    now showed dynamically

--- Code/App/www/js/controllers.js
+++ Code/App/www/js/controllers.js
@@ -22,13 +22,77 @@ angular.module('app.controllers', [])
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, $state, $ionicHistory) {
-    $scope.yieldAmount = new Array();
-    $scope.yieldAmount[2016] = 5;
-    $scope.yieldAmount[2017] = 6;
-    $scope.yieldAmount[2018] = 4;
-    $scope.yieldAmount[2019] = 6;
+    $scope.yieldForm = {};
+    $scope.yieldAmount = new Array();    
+    //mock data
+    $scope.cropTypes = ["Avocado", "Mango", "Litchi"];
+
+    $scope.data = [
+        {id: 9875131225687, name: "Fielie", amount: 5, cropType: "Mango"},
+        {id: 4613548984113, name: "Piet", amount: 4, cropType: "Mango"},
+        {id: 2354957878378, name: "Jan", amount: 6, cropType: "Mango"},
+        {id: 9874515568784, name: "Johan", amount: 6, cropType: "Mango"},
+        {id: 2123548789231, name: "Bota", amount: 2, cropType: "Avocado"},
+        {id: 1234567889852, name: "Moses", amount: 7, cropType: "Avocado"},
+        {id: 7851335459974, name: "Leshebogolo", amount: 6, cropType: "Avocado"},
+        {id: 2315484113321, name: "Manie", amount: 1, cropType: "Avocado"},
+        {id: 2345697452155, name: "Nkosi", amount: 9, cropType: "Avocado"},
+        {id: 7412589632588, name: "Zuma", amount: 4, cropType: "Litchi"},
+        {id: 6516787946131, name: "Malema", amount: 2, cropType: "Litchi"}
+    ];
+
+    /*Fetches data on load and processes the data to view in app*/
+    //gets the middle of the data for the two columns
+    $scope.middle = Math.ceil($scope.data.length/2);
+    $scope.calculateMiddle = function() {        
+        //gets the middle for number of elements containing cropType
+        var count = 0;
+        for(i in $scope.data) {
+            if($scope.data[i].cropType == $scope.yieldForm.selectedCrop)
+               count++;             
+        }
+
+        $scope.middle = Math.ceil(count/2);
+       // alert($scope.yieldForm.selectedCrop);
+    }
+    //two data arrays for the two columns
+    // $scope.col1 = new Array();
+    // $scope.col2 = new Array();
+
+    // for(i in data) {
+    //     if(i < middle)
+    //         $scope.col1.push(data[i]);
+    //     else
+    //         $scope.col2.push(data[i]);     
+    // }
+
+    function find(id, col) {
+        //Finds the foreman in the selected column and returns the index of the found foreman
+        var index = -1;
+        if(col == 1) {
+            for(i in $scope.col1) {
+                if($scope.col1[i].id == id) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+
+        if(col == 2) {
+            for(i in $scope.col2) {
+                if($scope.col2[i].id == id) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+
+        return index;
+    }
 
     $scope.logout = function() {
+        //Logs out the user
+        //When sessions are implemented the session will be cleared
         $state.go('splashScreen');
 
         return $ionicHistory.nextViewOptions({
@@ -40,14 +104,34 @@ function ($scope, $stateParams, $state, $ionicHistory) {
         $state.go('help');
     }
 
-    $scope.addOne = function(id) {
-        $scope.yieldAmount[id] += 1; 
+    $scope.addOne = function(id, col) {
+        var index = find(id, col);
+        if(index >= 0) {
+            if(col == 1) {
+                $scope.col1[index].amount += 1;
+            }
+
+            if(col == 2) {
+                $scope.col2[index].amount += 1;
+            }
+        } else alert("Cannot find foreman");
     }
 
-    $scope.removeOne = function(id) {
-        if($scope.yieldAmount[id] > 0)
-            $scope.yieldAmount[id] -= 1;
-        else $scope.yieldAmount[id] = 0;
+    $scope.removeOne = function(id, col) {
+        var index = find(id, col);
+        if(index >= 0) {
+            if(col == 1) {
+                if($scope.col1[index].amount > 0)
+                    $scope.col1[index].amount -= 1;
+                else $scope.col1[index].amount = 0;
+            }
+
+            if(col == 2) {
+                if($scope.col2[index].amount > 0)
+                    $scope.col2[index].amount -= 1;
+                else $scope.col2[index].amount = 0;
+            }
+        } else alert("Cannot find foreman");
     }
 }])
       
